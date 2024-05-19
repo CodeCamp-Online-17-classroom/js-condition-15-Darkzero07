902aed2204269858bca418eeab74504d6f14a099
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 11:51:01 +0700

    js lab 32

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// write code here
+if (browser === "Edge") {
+    alert( "You've got the Edge!" );
+    break;
+} else if (browser === "Chrome" || browser === "Firefox" || browser === "afari" || browser === "Opera" ) {
+    alert( 'Okay we support these browsers too' );
+    break;
+} else {
+    alert( 'We hope that this page looks ok!' );
+}
